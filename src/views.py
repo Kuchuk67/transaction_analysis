57b96d0b01952185b2807bab1ee46c047ac57a6f
Serg_Nout <kuchukov.sergey@gmail.com>
2024-10-15 07:39:21 +0300

    transactions_by_category

--- src/views.py
+++ src/views.py
@@ -1,3 +1,5 @@
+from src.utils import read_xls, financial_period, data_formater, filter_transaction
+
 def events() -> str:
     pass
 
@@ -21,9 +23,28 @@ def events() -> str:
 
 '''Стоимость акций'''
 
-def basic(data_transaction: dict, data_start: str, data_end: str, expenses: bool=True) -> dict:
-    '''Раздел «Основные», в котором траты(поступления) по категориям отсортированы по убыванию.
+def transactions_by_category(transactions: dict, expenses: bool=True) -> dict:
+    ''' Раздел «Основные», в котором траты(поступления) по категориям отсортированы по убыванию.
     Данные предоставляются по 7 категориям с наибольшими тратами
     (поступления  не ограниченно количеством категорий),
     траты по остальным категориям суммируются и попадают в категорию «Остальное».'''
-    pass
+    by_category: dict =  {}
+    for transaction in transactions:
+        category_in_tr = transaction.get('category')
+        category = by_category.get(category_in_tr, None)
+        if category:
+            by_category[category_in_tr] += transaction.get('payment_amount')
+        else:
+            by_category[category_in_tr] = transaction.get('payment_amount')
+    return by_category
+
+status,x = read_xls('operations.xlsx')
+#print(x[0])
+q = filter_transaction(x,'14.01.2021','14.11.2021')
+
+w = transactions_by_category(q)
+
+#print(*q, sep='\n')
+print(w)
+
+
